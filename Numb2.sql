e8f525d7418921120ace088e810029970eab7d69
Danis2019 <52890857+Danis2019@users.noreply.github.com>
2021-11-08 18:02:55 +0300

    Numb 2 and Numb 3

---
+++ Numb2.sql
@@ -0,0 +1,9 @@
+select name_author, count(name_author) as Популярность
+from author
+	inner join book_author using (author_id)
+	inner join book using(book_id)
+	inner join extradition using(book_id)
+where YEAR(extradition.date_issue) = 2007
+group by author
+order by Популярность desc
+LIMIT 1;
